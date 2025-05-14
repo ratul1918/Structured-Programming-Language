4210338ce02cfff4ebaf6bee5ebbd4f4ef4246a7
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-15 03:33:42 +0600

    Update pattern10.c

--- pattern10.c
+++ pattern10.c
@@ -1,22 +1,31 @@
-#include<stdio.h>
-int main(){
-    int i,j,rows,k=0;
+#include <stdio.h>
+
+int main() {
+    int i, j, rows, k = 0;
+
+    // Ask user to input the number of rows
     printf("Enter the number of rows: ");
-    scanf("%d",&rows);
+    scanf("%d", &rows);
 
-    for(i=1;i<=rows;i++){
+    // Loop through each row
+    for (i = 1; i <= rows; i++) {
+        // Increase k in first half, decrease in second half
+        if (i <= (rows + 1) / 2)
+            k++;
+        else
+            k--;
 
-        i<=(rows+1)/2?k++:k--;
-        for(j=1;j<=(rows+1)/2;j++){
-            if(j<=k){
-            printf("*");
-             } 
-             else{
-            printf(" ");
-             }
-        
+        // Print stars and spaces
+        for (j = 1; j <= (rows + 1) / 2; j++) {
+            if (j <= k)
+                printf("*");
+            else
+                printf(" ");
         }
+
+        // Move to next line after each row
         printf("\n");
-   
-}
+    }
+
+    return 0;
 }
