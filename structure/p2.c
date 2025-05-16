3a7a0f57444747016e0af224699836d7a6b93389
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-17 03:08:18 +0600

    Update p2.c

--- structure/p2.c
+++ structure/p2.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+
+/*
+    Write a C program that determines if a given number is "HAPPY" or "SAD".
+
+    A number is considered "HAPPY" if all of its digits are odd numbers.
+    If the number contains any even digit, it is considered "SAD".
+
+    Requirements:
+    - Read an integer input from the user (1 to 1000 inclusive).
+    - If the input is out of range, print "Error:Invalid input." and exit.
+    - Otherwise, check each digit of the number.
+    - If all digits are odd, print "HAPPY".
+    - If any digit is even, print "SAD".
+*/
+
 int main() {
     int num;
     scanf("%d", &num);
-    if (num<1 || num>1000){
+    if (num < 1 || num > 1000){
         printf("Error:Invalid input.\n");
-
         return 1;
     }
+
     int isHappy = 1;
     while (num > 0) {
         int digit = num % 10;
@@ -16,10 +31,12 @@ int main() {
         }
         num /= 10;
     }
+
     if (isHappy) {
         printf("HAPPY\n");
     } else {
         printf("SAD\n");
     }
+
     return 0;
 }
