d2583d0c01f3b4594f6e25a4fd9557889d9ecd84
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-22 04:08:26 +0600

    Update pattern4.c

--- starpattern/pattern4.c
+++ starpattern/pattern4.c
@@ -1,13 +1,22 @@
+// Question: Write a C program to print a pyramid pattern of asterisks (*) with 5 rows.
+// The pattern should look like:
+//     *    
+//    ***   
+//   *****  
+//  ******* 
+// *********
+
 #include<stdio.h>
 int main(){
-    int i,j;
-    for(i=1;i<=5;i++){
-        for(j=1;j<=9;j++){
-            if(j>=6-i && j<=4+i)
-            printf("*");
+    int i, j;
+    for(i = 1; i <= 5; i++){
+        for(j = 1; j <= 9; j++){
+            // Print '*' if current column is within the range for this row
+            if(j >= 6 - i && j <= 4 + i)
+                printf("*");
             else
-            printf(" ");
+                printf(" ");
         }
-        printf("\n");
+        printf("\n"); // Move to the next line after each row
     }
 }
