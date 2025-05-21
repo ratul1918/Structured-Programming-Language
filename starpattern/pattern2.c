bbe812aea3011ddfade0aaa74b43201134fb8b82
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-22 04:09:37 +0600

    Update pattern2.c

--- starpattern/pattern2.c
+++ starpattern/pattern2.c
@@ -1,15 +1,23 @@
+// Question: Write a C program to print a right-angled triangle of asterisks (*) aligned to the right.
+// The pattern should look like:
+// *****
+//  ****
+//   ***
+//    **
+//     *
+
 #include<stdio.h>
 int main(){
-    int i,j;
-    for(i=1;i<=5;i++){
-        for(j=1;j<=5;j++){
-            if(j>=i)
-            printf("*");
+    int i, j;
+    for(i = 1; i <= 5; i++){ // Loop through each row
+        for(j = 1; j <= 5; j++){ // Loop through each column in the row
+            // Print '*' if column is greater than or equal to the current row number
+            if(j >= i)
+                printf("*");
             else
-            printf(" ");
+                printf(" ");
         }
-        printf("\n");
+        printf("\n"); // Move to next line after each row
     }
-    
-    
 }
+
