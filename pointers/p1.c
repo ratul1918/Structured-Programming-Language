afebaa3d81485e34751728b5d57369fb19fc154c
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-23 03:21:03 +0600

    Update p1.c

--- pointers/p1.c
+++ pointers/p1.c
@@ -1,16 +1,19 @@
+// Question: Write a C program to demonstrate the use of pointers by printing the values and addresses
+// of variables x, y, and z using a single pointer variable.
+
 #include<stdio.h>
 int main(){
-    int x=5,y=10,z=30;
+    int x = 5, y = 10, z = 30;
 
-    int *p;
-    p=&x;
-    printf("x=%d\n",*p);
-    printf("x=%x\n",p);
+    int *p;         // Declare a pointer to int
 
-       p=&y;
-    printf("y=%d\n",*p);
+    p = &x;         // Pointer p now points to the address of x
+    printf("x=%d\n", *p);   // Print the value of x using pointer
+    printf("x=%x\n", p);    // Print the address of x in hexadecimal format
 
-       p=&z;
-    printf("z=%d\n",*p);
+    p = &y;         // Pointer p now points to the address of y
+    printf("y=%d\n", *p);   // Print the value of y using pointer
 
+    p = &z;         // Pointer p now points to the address of z
+    printf("z=%d\n", *p);   // Print the value of z using pointer
 }
