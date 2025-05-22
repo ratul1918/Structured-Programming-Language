8256927b81019c48d2a5dae7434265f97b607d2d
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-23 03:21:43 +0600

    Update p2.c

--- pointers/p2.c
+++ pointers/p2.c
@@ -1,13 +1,14 @@
+// Question: Write a C program to calculate the sum of three integers using pointers.
+
 #include<stdio.h>
 int main(){
-    int x=5,y=10,z=30,sum;
+    int x = 5, y = 10, z = 30, sum;
 
-    int *p1,*p2,*p3;
-    p1=&x;
-    p2=&y;
-    p3=&z;
+    int *p1, *p2, *p3;  // Declare three integer pointers
+    p1 = &x;            // p1 points to the address of x
+    p2 = &y;            // p2 points to the address of y
+    p3 = &z;            // p3 points to the address of z
 
-    sum=*p1 + *p2 + *p3;
-    printf("Sum= %d\n",sum);
-
-}    
+    sum = *p1 + *p2 + *p3;  // Dereference the pointers and add the values
+    printf("Sum= %d\n", sum);  // Print the sum
+}
