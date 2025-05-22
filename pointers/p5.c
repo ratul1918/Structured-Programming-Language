0b12354151125510ed8cadd373a2e0252287983d
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-23 03:23:06 +0600

    Update p5.c

--- pointers/p5.c
+++ pointers/p5.c
@@ -1,14 +1,18 @@
+// Question: Write a C program to print the elements of an array using pointers 
+// and also display their corresponding memory addresses.
+
 #include<stdio.h>
 int main(){
-    int a[5]={10,20,30,40,50};
-    int i,*p1;
+    int a[5] = {10, 20, 30, 40, 50};  // Declare and initialize an array
+    int i, *p1;
 
-    p1=&a[0];
+    p1 = &a[0];  // Initialize pointer to the first element of the array
 
-    for(i=0;i<5;i++){
-        printf("%d\n%x\n",*(p1+i),p1+i);
-        //*p1++;
+    for(i = 0; i < 5; i++){
+        // Print value using pointer arithmetic and dereferencing
+        // Print the memory address of each element
+        printf("%d\n%x\n", *(p1 + i), p1 + i);
 
+        // Optional: pointer increment using *p1++ is commented out
     }
-
 }
