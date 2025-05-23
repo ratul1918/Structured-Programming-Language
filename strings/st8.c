06ff0e3e5c251430e1785d122266c2c29a7441c9
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-24 03:36:03 +0600

    Update st8.c

--- strings/st8.c
+++ strings/st8.c
@@ -1,30 +1,40 @@
 #include<stdio.h>
 #include<string.h>
+
+// Question: Write a C program to check whether a given string is a palindrome or not 
+// by reversing the string manually and comparing it with the original.
+
 int main(){
-    char str1[50]="madam";
-    char str2[50];
-    int i=0,len=0,j;
-    while(str1[i]!='\0')
-    {
+    char str1[50] = "madam";    // Original string
+    char str2[50];              // To store the reversed string
+    int i = 0, len = 0, j;
+
+    // Calculate the length of the string manually
+    while(str1[i] != '\0') {
         i++;
         len++;
     }
-    for(j=0,i=len-1;i>=0;i--,j++)
-    {
 
-        str2[j]=str1[i];
+    // Reverse the string
+    for(j = 0, i = len - 1; i >= 0; i--, j++) {
+        str2[j] = str1[i];
     }
 
-    str2[j]='\0';
+    // Null-terminate the reversed string
+    str2[j] = '\0';
 
-    printf("str1=%s\n",str1);
-    printf("str2=%s\n",str2);
+    // Print both original and reversed strings
+    printf("str1 = %s\n", str1);
+    printf("str2 = %s\n", str2);
 
-    int d=strcmp(str1,str2);
-    if(d==0){
+    // Compare the original and reversed strings
+    int d = strcmp(str1, str2);
+    if(d == 0){
         printf("string is equal");
     }
-    else
-    printf("string is not equal");
+    else {
+        printf("string is not equal");
+    }
 
+    return 0;
 }
