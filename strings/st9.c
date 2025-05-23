35f3a09a57b3e61d16814752720898eca8a27020
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-24 03:36:44 +0600

    Update st9.c

--- strings/st9.c
+++ strings/st9.c
@@ -1,20 +1,25 @@
 #include<stdio.h>
 #include<string.h>
+
+// Question: Write a C program to swap the contents of two strings using a temporary string.
+
 int main(){
-    char str1[30]="Ratul";
-    char str2[20]="Rahman";
+    char str1[30] = "Ratul";
+    char str2[20] = "Rahman";
     char temp[40];
 
-    printf("before swapping\n");
-    printf("str1=%s\n",str1);
-    printf("str2=%s\n",str2);
+    printf("Before swapping\n");
+    printf("str1 = %s\n", str1);
+    printf("str2 = %s\n", str2);
 
-    strcpy(temp,str1);
-    strcpy(str1,str2);
-    strcpy(str2,temp);
-    printf("after swapping\n");
-    printf("str1=%s\n",str1);
-    printf("str2=%s",str2);
+    // Swapping the strings using a temporary string
+    strcpy(temp, str1);
+    strcpy(str1, str2);
+    strcpy(str2, temp);
 
+    printf("After swapping\n");
+    printf("str1 = %s\n", str1);
+    printf("str2 = %s", str2);
 
+    return 0;
 }
