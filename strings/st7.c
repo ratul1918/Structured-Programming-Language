39478d305d28b7ee3eddb6ce5819b8c3050211d3
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-24 03:35:23 +0600

    Update st7.c

--- strings/st7.c
+++ strings/st7.c
@@ -1,21 +1,31 @@
 #include<stdio.h>
 #include<string.h>
+
+// Question: Write a C program to reverse the contents of a string (without using strrev()) 
+// and store the result in another string.
+
 int main(){
-    char str1[50]="Hitman";
-    char str2[50];
-    int i=0,len=0,j;
-    while(str1[i]!='\0'){
+    char str1[50] = "Hitman";   // Original string
+    char str2[50];              // Reversed string
+    int i = 0, len = 0, j;
+
+    // Calculate the length of str1 manually
+    while(str1[i] != '\0'){
         i++;
         len++;
     }
-    for(j=0,i=len-1;i>=0;i--,j++){
 
-        str2[j]=str1[i];
-
+    // Reverse the string by copying characters from the end of str1 to the beginning of str2
+    for(j = 0, i = len - 1; i >= 0; i--, j++){
+        str2[j] = str1[i];
     }
-    str2[j]='\0';
 
-    printf("str1=%s\n",str1);
-    printf("str2=%s",str2);
+    // Null terminate the reversed string
+    str2[j] = '\0';
 
+    // Print both the original and reversed strings
+    printf("str1 = %s\n", str1);
+    printf("str2 = %s", str2);
+
+    return 0;
 }
