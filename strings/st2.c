4931ea0000cb4d3180d5b7994ddac99ffad7118f
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-24 03:31:27 +0600

    Update st2.c

--- strings/st2.c
+++ strings/st2.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
-int main(){
 
-    char s1[]="RAtul";
+int main() {
+    char s1[] = "RAtul"; // Declare and initialize a string
 
-    int i=0;
-    while(s1[i]!='\0'){
-        printf("%c\n",s1[i]);
+    int i = 0;
+    // Loop through each character until null character is reached
+    while (s1[i] != '\0') {
+        printf("%c\n", s1[i]); // Print one character per line
         i++;
     }
+
+    return 0;
 }
