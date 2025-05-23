019d75cfadee8ae645124469258b6a54063353e5
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-24 03:30:42 +0600

    Update st1.c

--- strings/st1.c
+++ strings/st1.c
@@ -1,9 +1,15 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
-    char str1[30];
-    printf("Enter your full name: ");
-    gets(str1);
+#include <stdio.h>
+#include <string.h>
 
-    printf("string1=%s",str1);
+int main() {
+    char str1[30];
+
+    printf("Enter your full name: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    // Remove trailing newline character added by fgets
+    str1[strcspn(str1, "\n")] = '\0';
+
+    printf("string1 = %s\n", str1);
+    return 0;
 }
