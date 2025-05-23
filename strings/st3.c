aff440c6a15531a2f425eaf3e615cc0147f3e1f4
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-24 03:32:06 +0600

    Update st3.c

--- strings/st3.c
+++ strings/st3.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<string.h>
-int main(){
-    char s1[]="Rafiur Rahman Ratul";
-    int len = strlen(s1);
-    printf("%d",len);
 
+int main() {
+    char s1[] = "Rafiur Rahman Ratul"; // Declare and initialize a string
+    int len = strlen(s1);              // Get the length of the string (excluding null terminator)
+    printf("%d", len);                 // Print the length
+    return 0;
 }
