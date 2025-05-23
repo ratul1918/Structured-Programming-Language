c63e042ad1ee41fa2e7b044107ad68f4a412cc6f
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-05-24 03:32:48 +0600

    Update st4.c

--- strings/st4.c
+++ strings/st4.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 #include<string.h>
-/*int main(){
-    char s1[]="Rafiur Rahman Ratul";
-    int len = strlen(s1);
-    printf("%d",len);
-}*/
+
 int main(){
-    char s1[]="Rafiur Rahman Ratul";
-    int i=0,len=0;
-    while(s1[i]!='\0'){
-        i++;
-        len++;
+    char s1[] = "Rafiur Rahman Ratul"; // Initialize string
+    int i = 0, len = 0;
+
+    while(s1[i] != '\0') {  // Loop until end of string
+        i++;                // Move to next character
+        len++;              // Count each character
     }
-    printf("Length=%d",len);
+
+    printf("Length = %d", len);  // Print string length
+    return 0;
 }
