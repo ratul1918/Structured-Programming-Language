4dd9f1d535baa7de4116cd5dd12972f612edce7e
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-06-29 23:28:20 +0600

    Update pattern13.c

--- pattern13.c
+++ pattern13.c
@@ -7,10 +7,10 @@ int main() {
     for(i = 1; i <= 7; i++) {
         k = 7 - i; // Initialize k to start decreasing values from 6 to 0
         
-        // Inner loop for each column in a row (total 7 columns)
+        // Inner loop for each column in a row (total 7 columns)..
         for(j = 1; j <= 7; j++) {
             if(j <= 8 - i) {
-                // Print current value of k followed by a space
+                // Print current value of k followed by a space..
                 printf("%d ", k);
                 k--; // Decrease k for next number
             } 
