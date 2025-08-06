057233dc34cf0f77788b1dcb4c1acc88e28ae7b5
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-08-07 02:46:23 +0600

    Update SPLFinalAssignment.c

--- SPLFinalAssignment.c
+++ SPLFinalAssignment.c
@@ -1,5 +1,4 @@
 // Function to input new student information.
-
 #include<Stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -396,5 +395,6 @@ void retrieve_from_file(struct student std[]) {
     printf("\t\tData has been retrieved from file successfully.\n\n");
     }
 }
+
 
 
