b4ec0a50d0bd3793552b85b4376d8e0c061966dd
Mr.Rafiur <120176812+ratul1918@users.noreply.github.com>
2025-12-25 01:01:30 +0600

    Add files via upload

---
+++ diagonalSum.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main() {
+    int n, m;
+
+    printf("Enter number of rows (n) and columns (m): ");
+    scanf("%d %d", &n, &m);
+
+    int arr[n][m];   
+    printf("Enter elements:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int totalSum = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(i == j){
+                totalSum += arr[i][j];
+            }
+        }
+    }
+
+    printf("Sum of all elements = %d\n", totalSum);
+
+    return 0;
+}
